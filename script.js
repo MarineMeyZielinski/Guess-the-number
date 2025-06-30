12c6c1e09fbc59ff2f5c03e415f30cf8379431e1
MarineMeyZielinski <m.zielinski.webdev@gmail.com>
2025-07-01 00:17:00 +0200

    Add JavaScript

--- script.js
+++ script.js
@@ -1,73 +1,69 @@
-//Générer un nombre aléatoire
-//Donner à l'utilisateur la capacité de deviner/jouer
-//Faire deviner l'utilisateur et s'il a faux, on lui fait recommencer le jeu en lui donnant des indications.
-//S'il gagne on stoppe
-
-//Création de la fonction guess
-// Nombre entre 0 et 20
-function guessGame() {
-  let randomNr = Math.floor(Math.random() * 21);
-  let guess;
-  console.log(randomNr);
-
-  do {
-    // guess = prompt("Devine un nombre entre 0 et 20");
-    // console.log(guess, randomNr);
-
-    // Si le nombre entré par l'utilisateur est + petit ou - grand :
-    if (randomNr > guess) {
-      console.log("C'est plus grand !");
-    } else if (randomNr < guess) {
-      console.log("C'est moins grand !");
-    }
-  } while (guess != randomNr);
-}
-
-//Appeler la fonction pour l'éxécuter
-guessGame();
-
-// Création de tableau pour stocker les nombrtes
-
-let numbers = [];
-
-// On sélectionne les éléments du DOM dont on a besoin
-
+// On récupère les éléments du DOM
 const numberInput = document.getElementById("numberInput");
 const addNumberBtn = document.getElementById("addNumberBtn");
 const numberList = document.getElementById("numberList");
+const essais = document.getElementById("essais");
+const answer = document.getElementById("answer");
+const newGame = document.getElementById("newGame");
 
-// On ajoute un écouteur d'événement sur le bouton "Ajouter"
+// const valeur = parseInt(numberInput.value, 10);
+let numbers = [];
+let nbEssais = 0;
+
+// Création du nombre aléatoire
+const randomNr = Math.floor(Math.random() * 20) + 1;
+console.log(randomNr);
+
+/*
+ *Création des actions du bouton "Vérifier"
+ *Création des comparaisons avec les conditions
+ *Création de la section essai
+ */
 
 addNumberBtn.addEventListener("click", () => {
-  //Récupère la valeur siasie et retire les espaces superflus
+  const newNumber = numberInput.value.trim();
+  nbEssais = nbEssais + 1;
+  essais.textContent = "Essai n°" + nbEssais;
 
-  const newNumber = numberInput.ariaValueMax.trim();
-
-  // Si la saisie n'est pas vide, on ajoute la tâche au tableau
-
-  if (newNumber !== "") {
+  if (newNumber >= 0 && newNumber <= 20 && newNumber !== "") {
     numbers.push(newNumber);
-
-    //On met à jour l'affichage des nombres
-
     renderNumbers();
+    numberInput.value = ""; // vider le champ après ajout
+
+    //Comparaisons
+    if (randomNr > newNumber) {
+      answer.textContent = "Tu chauffes... C'est plus grand !";
+      answer.style.color = "red";
+    } else if (randomNr < newNumber) {
+      answer.textContent = " Tu refroidis... C'est plus petit !";
+      answer.style.color = "red";
+    } else if (randomNr == newNumber) {
+      answer.textContent = " Bravo, tu as réussi !";
+      answer.style.color = "rgb(90, 255, 101)";
+      newGameFunc();
+    }
+  } else {
+    answer.textContent = "Entre un nombre compris entre 0 et 20 !";
+    answer.style.color = "red";
   }
 });
 
-/**
- * Fonction qui affiche la liste des nombres saisis à l'écran
- * Elle parcourt le tableau numbers et crée un <li> pour chaque élément
- */
+// Création de la fonction du bouton rejouer
+function newGameFunc() {
+  newGame.style.display = "block";
 
+  newGame?.addEventListener("click", () => {
+    window.location.reload();
+  });
+}
+
+//Création de la fonction pour créer les listes
 function renderNumbers() {
-  //On vide d'abbord la liste existante
-
   numberList.innerHTML = "";
 
-  // Pour chaque tâche dans le tableau...
-  numbers.forEach((number, index) => {
-    // ...on crée un élément de liste
+  numbers.forEach((number) => {
     const li = document.createElement("li");
     li.textContent = number;
+    numberList.appendChild(li);
   });
 }
